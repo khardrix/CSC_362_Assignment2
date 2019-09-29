50da64cf59e9895a5d27a24077e18bb93f44b138
khardrix <43048160+khardrix@users.noreply.github.com>
2019-09-29 17:11:19 -0400

    Add files via upload

--- Football.h
+++ Football.h
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_DEPRECATE
 #include <stdio.h>
 #include <math.h>
 
@@ -19,19 +20,28 @@
 // Used to read in each line of input from specified file and assign values to their respective variables
 void getInput(FILE*, char*, int*, int*, int*, int*, int*, char*, int*, int*, int*, int*);
 
+// Used to calculate the value of the double variable, predictor1, which is called in the compute(...){...} function 
 double computePredictionValue1(int, int);
+// Used to calculate the value of the double variable, predictor2, which is called in the compute(...){...} function
 double computePredictionValue2(int, int);
+// Used to calculate the value of the double variable, predictor3, which is called in the compute(...){...} function
 double computePredictionValue3(int, int);
+// Used to calculate the value of the double variable, predictor4, which is called in the compute(...){...} function
 double computePredictionValue4(int, int, int);
+// Used to calculate the value of the double variable, predictor5, which is called in the compute(...){...} function
 double computePredictionValue5(int, int, int, int, int, int);
-
+// Used to calculate all the values of the double variables, [predictor1, predictor2 ... predictor5]
 void compute(int, int, int, int, int, int, int, int, int, double *, double *, double *, double *, double *);
 
+// Used to calculate the predicted score differential of a football game 
 int getScore(double, double, double, double, double);
 
+// Used to print the predicted outcome of an individual football game
 void printOutput(int, char*, char*);
 
+// Used to update the int counter variable, totalGames. Also update homeWins, if the home team won
 void update(int, int *, int *);
 
+// Used to print a summary of all the football games predicted with the specific input file.
 void printSummary(int, int);
 /* -------------------------------------------- END OF FUNCTION PROTOTYPES ------------------------------------------ */
--- Main.c
+++ Main.c
@@ -1,8 +1,5 @@
-#define _CRT_SECURE_NO_DEPRECATE
-#define _CRT_SECURE_NO_WARNINGS
 #include "Football.h"
-#include <FootballComputation.c>
-#include <Football_IO.c>
+
 
 /*******************************************************************************************************************************************
  *******************************************************************************************************************************************
@@ -147,115 +144,6 @@ int main() {
 	// Exit the program and return 0 if no errors occurred 
 	return 0;
 }
-
-/*
-// This function gets one line of input from the football file currently being read, about individual games (one line of input per game)
-void getInput(FILE* fp, char* htn, int* hto, int* htd, int* hts, int* hth, int* hfa, char* vtn, int* vto, int* vtd, int* vts, int* vtr) {
-	fscanf(fp, "%s %d %d %d %d %d %s %d %d %d %d", htn, hto, htd, hts, hth, hfa, vtn, vto, vtd, vts, vtr);
-}
-*/
-/*
-// function for use in conjunction with the compute(...){...} function. 
-	// This function calculates the value of the double variable, predictor1
-double computePredictionValue1(int htOffense, int vtDefense) {
-	return ((htOffense * OFFENSIVE_FACTOR) - vtDefense);
-}
-
-
-// function for use in conjunction with the compute(...){...} function. 
-	// This function calculates the value of the double variable, predictor2
-double computePredictionValue2(int htDefense, int vtOffense) {
-	return ((htDefense + 2) - (vtOffense * OFFENSIVE_FACTOR));
-}
-
-
-// function for use in conjunction with the compute(...){...} function. 
-	// This function calculates the value of the double variable, predictor3
-double computePredictionValue3(int htSpecialTeams, int vtSpecialTeams) {
-	return ((htSpecialTeams * SPECIAL_TEAMS_FACTOR) - vtSpecialTeams);
-}
-
-
-// function for use in conjunction with the compute(...){...} function. 
-	// This function calculates the value of the double variable, predictor4
-double computePredictionValue4(int htHomeSTR, int htHomeADV, int vtSpecialTeams) {
-	return (htHomeSTR + (htHomeADV * HOME_FIELD_ADVANTAGE) - vtSpecialTeams);
-}
-
-
-// function for use in conjunction with the compute(...){...} function. 
-	// This function calculates the value of the double variable, predictor5
-double computePredictionValue5(int htOffense, int htDefense, int htHomeSTR, int vtOffense, int vtDefense, int vtRoadSTR) {
-	return ((htOffense * htDefense * htHomeSTR * OVERALL_FACTOR) - (vtOffense * vtDefense * vtRoadSTR));
-}
-
-
-// function used in conjunction with the getScore(...){...} function to help predict the winner of the game in question and
-	// the score differntial of said game.
-		// This function takes all the strengths of both teams playing a game of football and calculates values to be used
-			// by the getScore(...){...} function to predict the winner of the respective game, based upon the values assigned
-			// to the strengths of each team and the weights given to each strength field and respective formulas
-void compute(int htOffense, int htDefense, int htSpecialTeams, int htHomeSTR, int htHomeADV, int vtOffense, int vtDefense, int vtSpecialTeams, int vtRoadSTR, double* predictor1, double* predictor2, double* predictor3, double* predictor4, double* predictor5) {
-	*predictor1 = computePredictionValue1(htOffense, vtDefense);
-	*predictor2 = computePredictionValue2(htDefense, vtOffense);
-	*predictor3 = computePredictionValue3(htSpecialTeams, vtSpecialTeams);
-	*predictor4 = computePredictionValue4(htHomeSTR, htHomeADV, vtSpecialTeams);
-	*predictor5 = computePredictionValue5(htOffense, htDefense, htHomeSTR, vtOffense, vtDefense, vtRoadSTR);
-}
-
-
-// function used to predict the winner of the football game and the score differential of that game.
-	// Negative result = visiting team won by the absolute value of the calculated int
-	// Positive result = home team won by the absolute value of the calculated int
-int getScore(double predictor1, double predictor2, double predictor3, double predictor4, double predictor5) {
-	double difference = ((predictor1 * OFFENSIVE_WORTH) + (predictor2 * DEFENSIVE_WORTH) + (predictor3 * SPECIAL_WORTH) + (predictor4 * HOME_WORTH) + (predictor5 * OVERALL_WORTH));
-	return (int)difference;
-}
-*/
-/*
-// function used to print to the terminal the name of both teams, which team won and the amount of points the winning team won by
-	// Example output: "Cincinnati beat Buffalo by 3"
-void printOutput(int differential, char* homeTeamName, char* visitingTeamName) {
-	// if statement that checks if the two teams had a tie (score differential of 0)
-		// if there was a tie between the two teams, the home team is given the win by 1 (one) point
-	if (differential == 0) {
-		printf("%s beat %s by 1 \n", homeTeamName, visitingTeamName);
-	}
-	// else if statement that checks if the score differential was greater than 0 (zero)
-		// In this case, the home team won by a score differential equal to the int variable, differential
-	else if (differential > 0) {
-		printf("%s beat %s by %d \n", homeTeamName, visitingTeamName, differential);
-	}
-	// else statement that executes if the score differential was a negative number
-		// In this case, the visiting team won by the absolute value of the int variable, differential
-	else {
-		printf("%s beat %s by %d \n", visitingTeamName, homeTeamName, abs(differential));
-	}
-}
-*/
-/*
-// function used to update the total number of games predicted and if the home team is predicted to win,
-	// update the number of home teams predicted to win
-void update(int differential, int* totalGames, int* homeWins) {
-	// Increment the int counter variable, totalGames
-	*totalGames = *totalGames + 1;
-
-	// if statement that checks if the value of the calculated int variable, differential, is positive.
-		// If that is the case, increment the value of the int counter variable, homeWins
-	if (differential >= 0) {
-		*homeWins = *homeWins + 1;
-	}
-}
-*/
-/*
-// function used to print a summary of all the football games this program was used to predict the results for.
-	// This function calculates the percentage of games that home teams are predicted to win and 
-		// display that percentage (to 2 decimal places) along with the total number of games this program tried to predict
-void printSummary(int totalGames, int homeWins) {
-	double homeWinPercentage = (((double)homeWins / totalGames) * 100);
-	printf("\nOf %d games, %.2f%% were won by the home team \n\n\n\n", totalGames, homeWinPercentage);
-}
-*/
 
 
 /*
